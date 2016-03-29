307a826a97f71b7030d3bde1b8de2d912a9297b4
haosutang <haosutang@gmail.com>
2016-03-28 20:50:53 -0400

    hedge class with spy

--- work/Tang/Hedge.py
+++ work/Tang/Hedge.py
@@ -3,15 +3,22 @@ import numpy as np
 
 class Hedge(object):
     
-    def __init__(self, allstockdf, begindate = None, enddate = None):
+    def __init__(self, stocksdir = '../../data/all_stocks.csv', spydir = '../../data/spy.csv', begindate = None, enddate = None):
         # Use AAPL dates to filter
         # preprossessing
+        allstockdf = pd.read_csv(stocksdir)
+        spy = pd.read_csv(spydir)
+
         idxb = 0
         if begindate != None:
             idxb = np.where(allstockdf.Date>=begindate)[0][0]
         if enddate != None:
-            idxe = np.where(allstockdf.Date<=enddate)[0][0]
-            allstockdfsliced = allstockdf[idxb:idxe+1]
+            try:
+                idxe = np.where(allstockdf.Date>=enddate)[0][0]
+                allstockdfsliced = allstockdf[idxb:idxe+1]
+            except:
+                allstockdfsliced = allstockdf[idxb:]
+            
         else:
             allstockdfsliced = allstockdf[idxb:]
         
@@ -19,6 +26,10 @@ class Hedge(object):
         self.allstockdf = allstockdf.iloc[allstockdfsliced['AAPL'].dropna().index]
         
         self.allstockdf = self.allstockdf.dropna(axis = 1).reset_index(drop = True)
+
+        self._spy = spy[[i in self.allstockdf.Date.values for i in spy.Date]]
+
+        assert len(self._spy) == len(self.allstockdf)
         
         # initialize portfolio to be empty
         self._portfolio = []
@@ -43,3 +54,9 @@ class Hedge(object):
     @property
     def portfoliodf(self):
         return self.allstockdf[['Date']+list(self._portfolio)]
+
+    @property
+    def spy(self):
+        return self._spy
+
+    
