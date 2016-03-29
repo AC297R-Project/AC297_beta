54c2301f32c0d301a1bbd724129e69592812ca3d
christianjunge <christianjunge@g.harvard.edu>
2016-03-29 01:03:50 -0400

    End of the night

--- source/energy_functions.py
+++ source/energy_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def correlation(portfolio, spy):
-    return np.corrcoef(portfolio, spy)[0, 1]
+    return np.abs(np.corrcoef(portfolio, spy)[0, 1])
 
 def volatility(portfolio, spy=None):
     return portfolio.std()
